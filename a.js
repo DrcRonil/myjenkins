4b8390b80dcd8526ee8fabcbf411a35728253a38
DrcRonil <drcronil@gmail.com>
2021-07-10 18:04:16 +0530

    werwer

--- a.js
+++ a.js
@@ -1,25 +1,4 @@
 console.log('i m call.')
 
 
-console.log('i m call-1.')
-
-console.log('i m call-2.')
-
-console.log('i m call-2.')
-
-console.log('i m call-2.')
-console.log('i m call-2.')
-console.log('i m call-2.')
-console.log('i m call-2.')
-
-console.log('i m call-2.')
-
-console.log('i m call-2.')
-console.log('i m call-2.')
-console.log('i m call-2.')
-console.log('i m call-2.')
-
-
-console.log('i m call-2.')
-console.log('i m call-2.')
-console.log('i m call-2.')
+ 
